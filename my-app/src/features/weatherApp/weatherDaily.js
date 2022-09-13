28b614046fb067b037837bd8afde86fe8b160994
Levan D <97022558+Levan-D@users.noreply.github.com>
2022-09-13 11:35:40 +0400

    finished daily forecast

--- my-app/src/features/weatherApp/weatherDaily.js
+++ my-app/src/features/weatherApp/weatherDaily.js
@@ -5,14 +5,23 @@ import { useSelector, useDispatch } from "react-redux"
 import drop from "../../images/drop.png"
 
 const WeatherDaily = () => {
+  const weekdays = [
+    "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+  ]
+
   const Daily = useSelector(store => store.dailyWeather.data)
   const isLoading = useSelector(store => store.currentWeather.isLoading)
   if (isLoading === true) {
     return <div></div>
   }
 
-  let sortedTemp = {}
-  let count = {}
+  let count = []
   let filteredData = Daily.map(day => day.timeString.slice(8, 10))
   Daily.map((obj, i) => {
     if (obj.timeString.slice(8, 10) == filteredData[i]) {
@@ -21,16 +30,36 @@ const WeatherDaily = () => {
         : [obj]
     }
   })
-  let keys = Object.keys(count)
+  let sortedTemp = count.filter(a => a)
+  for (let i = 0; i < sortedTemp.length; i++) {
+    sortedTemp[i].sort((a, b) => (parseInt(a.temp) > parseInt(b.temp) ? 1 : -1))
+  }
 
-  for (let i = 0; i < keys.length; i++) {
-    sortedTemp[keys[i]] = count[keys[i]].sort((a, b) =>
-      parseInt(a.temp) > parseInt(b.temp) ? 1 : -1
+  let dailyRender = []
+
+  for (let i = 0; i < sortedTemp.length; i++) {
+    let weekdayIndex = new Date(sortedTemp[i][0].time * 1000).getDay()
+    let precip =
+      sortedTemp[i].map(data => parseInt(data.precip)).reduce((a, b) => a + b, 0) /
+      sortedTemp[i].length
+
+    dailyRender.push(
+      <div key={sortedTemp[i][0].id} className="dailyWeather">
+        <div className="weekday">{i === 0 ? `Today` : weekdays[weekdayIndex]} </div>
+        <div className="dailyPrecip">
+          <img src={drop} alt="rain drop icon" className="dropIcon" />
+          <div> {precip.toFixed()} %</div>
+        </div>
+        <div className="dailytemp">
+          {sortedTemp[i].length > 1
+            ? `${sortedTemp[i][0].temp} ${sortedTemp[i][sortedTemp[i].length - 1].temp}`
+            : sortedTemp[i][0].temp}{" "}
+        </div>
+      </div>
     )
   }
-  console.log(count)
 
-  return <div className="hourlyCardContainer">aaaa</div>
+  return <div className="DailyCardContainer">{dailyRender}</div>
 }
 
 export default WeatherDaily
